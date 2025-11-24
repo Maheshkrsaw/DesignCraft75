572a97911b2b72d0d4cdca7848d3275df412e55e
Mahesh Kumar <maheshkumar9304631853@gmail.com>
2025-11-24 15:22:34 +0530

    project done but not responsive yet

---
+++ 07-Porsche/script.js
@@ -0,0 +1,61 @@
+const scrollRevealOption = {
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+};
+
+ScrollReveal().reveal('.container .letter-s', {
+    duration: 1000,
+    delay: 1000,
+});
+
+ScrollReveal().reveal('.container img', {
+    duration: 1000,
+    delay: 1500,
+});
+
+
+ScrollReveal().reveal('.container .text_left', {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 2000,
+});
+
+ScrollReveal().reveal('.container .text_right', {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 2000,
+});
+
+
+ScrollReveal().reveal('.container .explore', {
+    duration: 1000,
+    delay: 2500,
+});
+
+
+ScrollReveal().reveal('.container h5', {
+    interval: 500,
+    duration: 1000,
+    delay: 3000,
+});
+
+ScrollReveal().reveal('.container .catalog', {
+    duration: 1000,
+    delay: 3500,
+});
+
+
+ScrollReveal().reveal('.container .print', {
+    duration: 1000,
+    delay: 5500,
+});
+
+
+ScrollReveal().reveal('.footer p', {
+    duration: 1000,
+    delay: 7000,
+});
+
+
+
