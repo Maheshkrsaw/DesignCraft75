f17c084e383152ca7a74068080592e4dbeaafd47
Mahesh Kumar <maheshkumar9304631853@gmail.com>
2025-11-25 14:01:49 +0530

    website done

--- 19-PLAY-landing-pages/script.js
+++ 19-PLAY-landing-pages/script.js
@@ -20,23 +20,23 @@ const scrollRevealOption = {
   duration: 1000,
 };
 
-ScrollReveal().reveal(".header__image img", {
+ScrollReveal().reveal(".header_image img", {
   ...scrollRevealOption,
   origin: "right",
 });
-ScrollReveal().reveal(".header__content h2", {
+ScrollReveal().reveal(".header_content h2", {
   ...scrollRevealOption,
   delay: 500,
 });
-ScrollReveal().reveal(".header__content h1", {
+ScrollReveal().reveal(".header_content h1", {
   ...scrollRevealOption,
   delay: 1000,
 });
-ScrollReveal().reveal(".header__content p", {
+ScrollReveal().reveal(".header_content p", {
   ...scrollRevealOption,
   delay: 1500,
 });
-ScrollReveal().reveal(".header__btns", {
+ScrollReveal().reveal(".header_btns", {
   ...scrollRevealOption,
   delay: 2000,
 });
