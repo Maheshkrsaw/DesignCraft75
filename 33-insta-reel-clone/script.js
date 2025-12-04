b37286c897d034dc3642d5ebc7df4747d45409cc
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-05 04:43:34 +0530

    udated days  till  33

--- 33-insta-reel-clone/script.js
+++ 33-insta-reel-clone/script.js
@@ -1,3 +1,69 @@
+const reelsFeed = document.getElementById("reelsFeed");
+
+const users = ["cat_daily", "travel_vlog", "doggo_life", "nature_love", "tech_world"];
+const captions = [
+  "When you realize it's Friday! 😺💃",
+  "Running into the weekend like... 😂",
+  "My dog thinks he is human! 🐶👔",
+  "Nature always wins 🌿",
+  "Coding all night 💻🔥"
+];
+
+for (let i = 1; i <= 5; i++) {
+  const reel = document.createElement("div");
+  reel.classList.add("reel");
+
+  reel.innerHTML = `
+    <video class="reel-video" loop muted playsinline src="asset/${i}.mp4"></video>
+
+    <div class="mute-overlay">
+      <i class="fa-solid fa-volume-xmark" style="font-size: 30px;"></i>
+    </div>
+
+    <div class="actions-col">
+      <div class="action-btn like-btn">
+        <i class="fa-solid fa-heart"></i>
+        <span class="action-text">${Math.floor(Math.random() * 100)}K</span>
+      </div>
+      <div class="action-btn">
+        <i class="fa-regular fa-comment-dots"></i>
+        <span class="action-text">${Math.floor(Math.random() * 1000)}</span>
+      </div>
+      <div class="action-btn">
+        <i class="fa-regular fa-paper-plane"></i>
+      </div>
+      <div class="action-btn">
+        <i class="fa-solid fa-ellipsis-vertical"></i>
+      </div>
+
+      <div class="music-disc">
+        <img src="https://source.unsplash.com/50x50/?music,random&sig=${i}">
+      </div>
+    </div>
+
+    <div class="reel-info">
+      <div class="user-row">
+<img src="https://picsum.photos/50/50?random=${i}" class="user-thumb">
+        <span class="username">${users[i - 1]}</span>
+        <button class="follow-btn">Follow</button>
+      </div>
+
+      <div class="caption">${captions[i - 1]}</div>
+
+      <div class="music-row">
+        <i class="fa-solid fa-music" style="font-size: 10px;"></i>
+        <marquee scrollamount="4" style="width:150px;">
+          Original Audio • Trending
+        </marquee>
+      </div>
+    </div>
+  `;
+
+  reelsFeed.appendChild(reel);
+}
+
+
+
 // Video Logic
 const videos = document.querySelectorAll('video');
 const observer = new IntersectionObserver((entries) => {
