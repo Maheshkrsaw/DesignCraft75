08df254675e895a2e8e3e80a00b7f58eba7ca9c1
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-10 19:21:04 +0530

    updating

--- JS-ONE-SHOT-HITESH-CHODHARY/test.js
+++ JS-ONE-SHOT-HITESH-CHODHARY/test.js
@@ -1,5 +1,21 @@
-let linkedin="MAhesh"
+// let linkedin="MAhesh"
+// let youtube=linkedin
+// youtube="Mahi"
+// console.log(linkedin);
+// console.log(youtube);
+
+
+
+let linkedin = {
+    id:1,
+    name:"Mahesh"
+}
+
 let youtube=linkedin
-youtube="Mahi"
+
+youtube.name="mahi"
+
+
 console.log(linkedin);
 console.log(youtube);
+
