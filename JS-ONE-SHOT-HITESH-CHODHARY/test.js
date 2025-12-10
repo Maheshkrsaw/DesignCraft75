103ee9bece943379d8480a367e1c52c9ba5697db
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-10 18:33:49 +0530

    updating

--- JS-ONE-SHOT-HITESH-CHODHARY/test.js
+++ JS-ONE-SHOT-HITESH-CHODHARY/test.js
@@ -1,9 +1,3 @@
-  let mahesh="mahesh"
-  let mahesh2="mahesh2"
-  let mahesh3="mahesh3"
-  let mahesh4="mahesh4"
-  const Mahi=12345
-  var mah=5896
-  const islogged=true
+let score="33abc"
 
-  console.table({mahesh,mahesh2,mahesh3,mahesh4,Mahi,mah,islogged})
+console.log(typeof score)
