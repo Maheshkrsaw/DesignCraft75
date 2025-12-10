108e8cd21ac78aefc1497809f5e38c47fe599969
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-10 19:06:11 +0530

    updating

--- JS-ONE-SHOT-HITESH-CHODHARY/test.js
+++ JS-ONE-SHOT-HITESH-CHODHARY/test.js
@@ -1,9 +1,7 @@
-let x=10
-x="mahesh "
-x=23
-console.log(x);
-console.table({x})
+
 
 
 let useremauil;
-console.log(typeof useremauil);
+// console.log(typeof useremauil);
+console.log(useremauil);
+
