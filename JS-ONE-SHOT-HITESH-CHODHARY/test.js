113775fb7f95772a165efebd97b15da2e1852dd2
Maheshkrsaw <maheshkumar9304631853@gmail.com>
2025-12-10 19:08:31 +0530

    updating

--- JS-ONE-SHOT-HITESH-CHODHARY/test.js
+++ JS-ONE-SHOT-HITESH-CHODHARY/test.js
@@ -3,5 +3,10 @@
 
 let useremauil;
 // console.log(typeof useremauil);
-console.log(useremauil);
+// console.log(useremauil);
 
+
+
+const id=Symbol(122)
+const anotherId=Symbol(122)
+console.log(id===anotherId);
